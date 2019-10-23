f5360ec42245f4568570f401a5811f7cea5688f7
nick12340 <37206316+nick12340@users.noreply.github.com>
2019-10-23 15:09:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,13 +16,6 @@ warnings.filterwarnings("ignore")
 def train_model(model, X_train, y_train, name, config):
     """train
     train a single model.
-
-    # Arguments
-        model: Model, NN model to train.
-        X_train: ndarray(number, lags), Input data for train.
-        y_train: ndarray(number, ), result data for train.
-        name: String, name of model.
-        config: Dict, parameter for train.
     """
 
     model.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
@@ -38,46 +31,7 @@ def train_model(model, X_train, y_train, name, config):
     df.to_csv('model/' + name + ' loss.csv', encoding='utf-8', index=False)
 
 
-def train_seas(models, X_train, y_train, name, config):
-    """train
-    train the SAEs model.
-
-    # Arguments
-        models: List, list of SAE model.
-        X_train: ndarray(number, lags), Input data for train.
-        y_train: ndarray(number, ), result data for train.
-        name: String, name of model.
-        config: Dict, parameter for train.
-    """
-
-    temp = X_train
-    # early = EarlyStopping(monitor='val_loss', patience=30, verbose=0, mode='auto')
-
-    for i in range(len(models) - 1):
-        if i > 0:
-            p = models[i - 1]
-            hidden_layer_model = Model(input=p.input,
-                                       output=p.get_layer('hidden').output)
-            temp = hidden_layer_model.predict(temp)
-
-        m = models[i]
-        m.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
-
-        m.fit(temp, y_train, batch_size=config["batch"],
-              epochs=config["epochs"],
-              validation_split=0.05)
-
-        models[i] = m
-
-    saes = models[-1]
-    for i in range(len(models) - 1):
-        weights = models[i].get_layer('hidden').get_weights()
-        saes.get_layer('hidden%d' % (i + 1)).set_weights(weights)
-
-    train_model(saes, X_train, y_train, name, config)
-
-
-def main(argv):
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model",
@@ -106,4 +60,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
